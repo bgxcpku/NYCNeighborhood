9f7298f90a5910c95ae10a6b1afee23dfe2176e7
Laurent <laurent.bouin@gmail.com>
2012-09-04 12:05:37 -0400

    Commit of public dir

--- app/services/FoursquareServicePlayImp.java
+++ app/services/FoursquareServicePlayImp.java
@@ -1,9 +1,17 @@
 package services;
 
-/**
- * User: lbouin
- * Date: 04/09/12
- * Time: 11:41
- */
-public class FoursquareServicePlayImp {
+import play.libs.WS;
+import util.URLHelper;
+
+public class FoursquareServicePlayImp implements FoursquareService{
+
+    @Override
+    public void callReply(String checkinId, String userToken, String replyText) {
+
+        play.Logger.info("Calling foursquare reply endPoind: checkinId:%s,userToken:%s,reply:%s",checkinId,userToken,replyText);
+
+        String replyUrl = URLHelper.buildReplyUrl(checkinId, replyText, userToken);
+        String replyAnswer =  WS.url(replyUrl).post().getString();
+
+    }
 }
